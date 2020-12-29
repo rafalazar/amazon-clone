5a39374f19ccefae6df0fe582f80a469ce4139bc
Jordan <jcr.salazar@outlook.com>
2020-12-29 18:54:58 -0500

    Se crea el componente Product

---
+++ src/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Product.css';
+
+const Product = () => {
+    return(
+        <div className="product">
+
+        </div>
+    );
+}
+
+export default Product;
