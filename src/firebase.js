575a338b51793a082278588120f25599d2274796
Jordan <jcr.salazar@outlook.com>
2020-12-29 18:54:35 -0500

    Configuraciones Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDgkXffpz6QMCkFuD3o5vjt84PprgWX_N4",
+    authDomain: "clone-3ae96.firebaseapp.com",
+    projectId: "clone-3ae96",
+    storageBucket: "clone-3ae96.appspot.com",
+    messagingSenderId: "594238345456",
+    appId: "1:594238345456:web:bc77be91c20a68674e3235",
+    measurementId: "G-8MVPJMYH12"
+  };
