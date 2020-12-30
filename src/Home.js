8d39c4dc84cbc4cc49c6d9e6be86ca4f42a098f1
Jordan <jcr.salazar@outlook.com>
2020-12-30 12:08:38 -0500

    Home Page done

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 const Home = () => {
     return (
@@ -12,18 +13,54 @@ const Home = () => {
                 />
 
                 <div className="home__row">
-                    {/* Product */}
-                    {/* Product */}
+                    <Product
+                        id="1234501"
+                        title="The Last of Us Part II - PlayStation 4 Ellie Edition"
+                        price={419.95}
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/91alt4X%2BC4L._SL1500_.jpg"
+                    />
+                    <Product
+                        id="1234502"
+                        title="Razer Blade 15 Gaming Laptop - Intel Core i7-8750H 6 Core, GeForce RTX 2060, 15.6 FHD 144Hz, 16GB RAM, 512GB SSD, teclado Chroma RGB, Thunderbolt 3, 0.70"
+                        price={1899.88}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61LemXk5tuL._AC_SX679_.jpg"
+                    />
                 </div>
 
                 <div className="home__row">
-                    {/* Product */}
-                    {/* Product */}
-                    {/* Product */}
+                    <Product
+                        id="1234503"
+                        title="Oficial Microsoft Xbox One Wireless Controller Black"
+                        price={61.99}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/41LO2OX6pRL._SX466_.jpg" 
+                    />
+                    <Product
+                        id="1234504"
+                        title="The Art of Game of Thrones, the official book of design from Season 1 to Season 8" 
+                        price={44.25} 
+                        rating={5} 
+                        image="https://images-na.ssl-images-amazon.com/images/I/51Lza3JLgOL._SX381_BO1,204,203,200_.jpg"
+                    />
+                    <Product
+                        id="1234505"
+                        title="Logitech G502 Lightspeed Wireless Mouse HERO Sensor 25K, PowerPlay and Lightsync RGB - black"
+                        price={99.99}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/71uNZAdQOoL._AC_SL1500_.jpg"
+                    />
                 </div>
                 
                 <div className="home__row">
-                    {/* Product */}
+                    <Product
+                        id="1234506"
+                        title="SAMSUNG LC49RG90SSNXZA CRG9 - Monitor de juego curvado, color negro"
+                        price={1049.99}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/71tZW1aa%2BPL._AC_SL1500_.jpg"
+                    />
                 </div>
 
 
--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Product.css';
 
-const Product = () => {
+const Product = ({id, title, image, price, rating}) => {
     return(
         <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
 
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_,i) => (
+                            <p>⭐</p>
+                    ))}
+                </div>
+            </div>
+
+            <img src={image} alt="Producto"/>
+
+            <button>Add to Bag</button>
         </div>
     );
 }
