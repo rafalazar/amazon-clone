da511f057156d53df59ccd1cc5fb73e7c666f46c
Jordan <jcr.salazar@outlook.com>
2020-12-30 12:41:08 -0500

    Router added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App = () => {
 	return (
-		<div className="app">
-			<Header />
-			<Home />
-		</div>
+		<Router>
+			<div className="app">
+				<Switch>
+
+					<Route path="/checkout">
+						<Header />
+						<h1>I am the Checkout Page :3</h1>
+					</Route>
+					<Route path="/">
+						<Header />
+						<Home />
+					</Route>
+
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
