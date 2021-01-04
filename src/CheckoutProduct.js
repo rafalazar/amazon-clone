78b1ae68b69cc9d28757c26e35c4cbc0e131e1d5
Jordan <jcr.salazar@outlook.com>
2021-01-04 18:26:24 -0500

    CheckoutProduct componente created

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CheckoutProduct.css';
+
+const CheckoutProduct = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CheckoutProduct;
