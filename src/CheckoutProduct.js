31daa4e1a37d8dc7147b3060eae8b50fb4648b71
Jordan <jcr.salazar@outlook.com>
2021-01-04 18:35:12 -0500

    CheckoutProduct avanzado - en desarrollo

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './CheckoutProduct.css';
 
-const CheckoutProduct = () => {
+const CheckoutProduct = ({id, image, title, price, rating}) => {
     return (
-        <div>
-            
+        <div className="checkoutProduct">
+            <img className="checkoutProduct__image" src={image} alt="Product image"/>
+            <div className="checkoutProduct__info">
+                <p className="checkoutProduct__title">{title}</p>
+                <p className="checkoutProduct__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+            </div>
         </div>
     );
 }
