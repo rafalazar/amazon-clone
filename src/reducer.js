af5fd0ed22797c91eab6c442fcce1d30e826e40c
Jordan <jcr.salazar@outlook.com>
2021-01-05 11:07:08 -0500

    Remove from basket function added

--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,6 @@ export const getBasketTotal = (basket) =>
     basket?.reduce((amount, item) => item.price + amount, 0);
 
 
-
 const reducer = (state, action) => {
     console.log(action);
     switch(action.type){
@@ -16,6 +15,26 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item],
             };
+
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+
+            if(index >= 0){
+                newBasket.splice(index, 1);
+
+            }
+            else {
+                console.warn(`Can't remove product (id: ${action.id}) as it's not in Basket!`);
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+
         default:
             return state;
     }
