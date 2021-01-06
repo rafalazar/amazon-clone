e4f664e49eee35e4ecc15b0e7e6510ec5adc680b
Jordan <jcr.salazar@outlook.com>
2021-01-06 14:39:03 -0500

    SignIn and SignOut click event

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,17 @@ import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 const Header = () => {
 
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
+
+    const handleAuth = () => {
+        if(user) {
+            auth.signOut();
+        }
+    }
 
     return (
         <div className="header">
@@ -25,14 +32,16 @@ const Header = () => {
             </div>
 
             <div className="header__nav">
-                <div className="header__option">
-                    <span className="header__optionLineOne">
-                        Hello Guest
-                    </span>
-                    <span className="header__optionLineTwo">
-                        Sign In
-                    </span>
-                </div>
+                <Link to={!user && '/login'}>
+                    <div onClick={handleAuth} className="header__option">
+                        <span className="header__optionLineOne">
+                            Hello Guest
+                        </span>
+                        <span className="header__optionLineTwo">
+                            {user ? 'Sign Out' : 'Sign In'}
+                        </span>
+                    </div>
+                </Link>
                 <div className="header__option">
                     <span className="header__optionLineOne">
                         Returns
