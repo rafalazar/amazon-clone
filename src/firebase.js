615856c10d128e115cc4cca62665b4ceba88351e
Jordan <jcr.salazar@outlook.com>
2021-01-06 14:37:50 -0500

    firebase configurations added

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDgkXffpz6QMCkFuD3o5vjt84PprgWX_N4",
     authDomain: "clone-3ae96.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:594238345456:web:bc77be91c20a68674e3235",
     measurementId: "G-8MVPJMYH12"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+  const auth = firebase.auth();
+
+  export { db, auth};
